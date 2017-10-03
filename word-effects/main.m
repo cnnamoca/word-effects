db76ec37dc4706b75edad61fca80c76cbaaa0a75
Carlo <carlo.namoca@gmail.com>
2017-10-02 19:04:09 -0700

    Added case 5

--- word-effects/main.m
+++ word-effects/main.m
@@ -37,27 +37,48 @@ int main(int argc, const char * argv[]) {
         char inputChars[255];
         printf ("input a string:");
         fgets (inputChars, 255, stdin);
-        printf ("your string is %s \n", inputChars);
-        NSString *inputString = [NSString stringWithUTF8String: inputChars];
+        printf ("your string is: %s \n", inputChars);
+        NSString *tempInputString = [NSString stringWithUTF8String: inputChars];
+        NSString *inputString = [tempInputString stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+        
         
         printf ("1: Uppercase\n");
         printf ("2: Lowercase\n");
         printf ("3: Numberize (Write a number and I'll convert it into an integer!)\n");
         printf ("4: Canadianize\n");
-        printf ("5: Respond\n");
+        printf ("5: Respond (Try adding a '?' or '!' in the end of your sentence \n");
         printf ("6: De-space-it\n\n");
         
         printf ("Select an option:\n");
         scanf("%d", &choice);
         
+        
+        
         switch(choice){
             case 1: NSLog (@"Input was %@", [inputString uppercaseString]); break;
+            
             case 2: NSLog (@"Input was %@", [inputString lowercaseString]); break;
             
-            //still need to check this
             case 3: NSLog (@"Input was %ld", (long)[inputString integerValue]); break;
 
-            case 4: NSLog (@"Input was %@", [inputString stringByAppendingString:@"eh"]); break;
+            case 4:
+                NSLog (@"Input was %@", [inputString stringByAppendingString:@"eh"]);
+                break;
+            
+            case 5:
+            //REMEMBER TO PUT {} FOR CASES LIKE THESE
+            {
+                
+                NSString *lastCharacter = [inputString substringFromIndex:[inputString length] - 1];
+                if ([lastCharacter isEqualToString:@"!"]) {
+                    NSLog (@"Whoa, calm down!");
+                    break;
+                } else if ([lastCharacter isEqualToString:@"?"]) {
+                    NSLog (@"I don't know :/");
+                }
+                break;
+                
+            }
             
             case 6: NSLog (@"Input was %@", [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"]); break;
             
