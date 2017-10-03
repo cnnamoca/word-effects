bb74e69dcdc1aaab6e5ddbfeb70a3c30649b7171
Carlo <carlo.namoca@gmail.com>
2017-10-02 17:25:25 -0700

    First commit

---
+++ word-effects/word-effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  word-effects
+//
+//  Created by Carlo Namoca on 2017-10-02.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
